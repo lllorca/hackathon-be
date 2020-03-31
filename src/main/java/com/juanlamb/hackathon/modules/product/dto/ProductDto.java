883740062be73029b523f6c515efb08a968c9cb3
lllorca <lllorca@itba.edu.ar>
2020-03-31 17:56:02 +0200

    Quick fix

--- src/main/java/com/juanlamb/hackathon/modules/product/dto/ProductDto.java
+++ src/main/java/com/juanlamb/hackathon/modules/product/dto/ProductDto.java
@@ -1,13 +1,11 @@
 package com.juanlamb.hackathon.modules.product.dto;
 
-import com.juanlamb.hackathon.modules.business.dto.BusinessDto;
 import lombok.Data;
 
 @Data
 public class ProductDto {
 
     private long id;
-    private BusinessDto business;
     private String name;
     private double price;
     private String paymentUrl;
